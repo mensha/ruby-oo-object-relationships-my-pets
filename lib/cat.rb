d6248c3146b18b8f6e6e682952b993049a121502
mensha <msksalston@gmail.com>
2020-12-22 16:03:45 -0500

    1

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Cat
-  # code goes here
+    attr_reader :name
+    attr_accessor :owner, :mood
+     @@cat = []
+        def initialize(name, owner)
+          @name = name
+          @owner = owner
+          @mood = "nervous"
+          @@cat << self
+        end
+
+
+        def self.all
+          @@cat
+        end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,81 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor  
+   @@all = []
+
+        def initialize(name, species = "human")
+        @name = name
+        @species = species
+        @@all << self
+         end
+
+        def say_species        
+         return "I am a human."
+        end
+
+        def self.all
+          @@all
+        end
+
+        def self.count
+          @@all.size do |owner| 
+          end
+        end
+
+        def self.reset_all
+          @@all.clear
+          end
+        
+
+          def cats 
+            Cat.all.select {|cat| cat.owner == self}                    
+
+          end
+
+          def dogs 
+            Dog.all.select {|dog| dog.owner == self}                    
+  
+          end
+
+          def buy_cat(name)
+            new_cat = Cat.new(name, self)
+           
+          end
+
+
+          def buy_dog(name)
+            new_dog = Dog.new(name, self)
+          end
+
+          def walk_dogs            
+              self.dogs.each do |dog| dog.mood = "happy"
+               end
+          end
+
+          def feed_cats
+                  self.cats.each do |cat| cat.mood = "happy"
+                   end
+          end
+
+
+            def pets          
+            self.dogs + self.cats
+                           
+
+          end
+
+          def sell_pets       
+                self.pets.each do |pet| 
+                  pet.mood = "nervous"
+                  pet.owner = nil                
+                end
+
+          end
+
+          def list_pets
+             x_dogs = self.dogs.count
+             x_cats = self.cats.count
+             return "I have #{x_dogs} dog(s), and #{x_cats} cat(s)."             
+
+          end
 end
